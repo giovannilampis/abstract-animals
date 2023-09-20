9362a6ed63d448030d3359521c04891e405869ab
giovannilampis <giolampis@hotmail.com>
2023-09-20 15:06:35 +0200

    add content to Animale class

--- src/org/pack/abs/Animale.java
+++ src/org/pack/abs/Animale.java
@@ -2,4 +2,25 @@ package org.pack.abs;
 
 public abstract class Animale {
 
+	private String name;
+	
+	public Animale(String name) {
+		
+		setName(name);
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	@Override
+	public String toString() {
+		
+		return getName()  
+				
+		;
+	}
 }
